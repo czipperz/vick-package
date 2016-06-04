49806d0b60d6e2306eaea179877397160ecda901
Czipperz <czipperz@gmail.com>
2016-06-04 13:49:22 -0700

    License lib.hh with MPL by default

--- src/create_package.cc
+++ src/create_package.cc
@@ -464,7 +464,10 @@ script:
             }
         }
         all_caps = "HEADER_GUARD_" + all_caps + "_H";
-        f << "#ifndef " << all_caps << endl
+        f << R"(/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */)" "\n" << endl
+          << "#ifndef " << all_caps << endl
           << "#define " << all_caps << endl
           << '\n'
           << "/*!\n * \\file " << dirname
